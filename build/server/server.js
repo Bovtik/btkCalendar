551e12ddd7680ff64b04f78342204d540f9c8a6b
Bovtik <bovtik.work@gmail.com>
2016-05-16 17:35:19 +0300

    Tweaked server app launching - you now can pass port and ip-adress

--- build/server/server.js
+++ build/server/server.js
@@ -23,7 +23,9 @@ server.get('/', function (req, res) {
   res.end("<!DOCTYPE html>" + html);
 });
 
-//server.listen(process.env.PORT, '0.0.0.0');
-server.listen(80);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-console.log('Server running at http://localhost:80/');
+server.listen(server_port, server_host, function () {
+  console.log('Listening on port %d', server_port);
+});
